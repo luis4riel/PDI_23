4af05baba388c49e68dd98fdcf9db9a5e6957e26
luis <zluisariel@gmail.com>
2023-09-04 18:24:18 -0300

    feat-init product model

--- Arquitetura Microservices/S07 - Shopping/Shopping.API/Program.cs
+++ Arquitetura Microservices/S07 - Shopping/Shopping.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Shopping.API.Model.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection, new MySqlServerVersion(new Version(8, 0, 34)))
+);
 
 var app = builder.Build();
 
